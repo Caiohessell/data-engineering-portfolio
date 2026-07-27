30d2c0af289e96c13860d56ed0bcae34a088519c
Caiohessell <caiohessel245@gmail.com>
2026-07-27 16:43:29 -0300

    Adicionando consultas e README

---
+++ projetos/students_mental_health/sql/limpeza.sql
@@ -0,0 +1,52 @@
+-- Quantos registros existem na tabela?
+
+SELECT COUNT(*) 
+FROM students;
+
+
+-- Quais colunas existem?
+
+SELECT *
+FROM students
+LIMIT 5;
+
+
+-- Existem valores nulos nas colunas utilizadas na análise?
+
+SELECT
+    COUNT(*) AS total_registros,
+    COUNT(todep) AS total_reg_depressao,
+    COUNT(age) AS total_reg_idade,
+    COUNT(gender) AS total_reg_genero,
+    COUNT(inter_dom) AS total_reg_tipo
+FROM students;
+
+
+-- Existem categorias escritas de maneiras diferentes?
+
+SELECT DISTINCT gender
+FROM students;
+
+SELECT DISTINCT inter_dom
+FROM students;
+
+SELECT DISTINCT academic
+FROM students;
+
+SELECT DISTINCT todep
+FROM students;
+
+
+-- Qual a idade mínima e a máxima no conjunto?
+
+SELECT MIN(age) AS  idade_minima, MAX(age) AS idade_maxima
+FROM students;
+
+
+-- Como estão distribuídas as idades?
+
+SELECT age AS idade, COUNT(age) AS total_idade
+FROM students
+WHERE age IS NOT NULL
+GROUP BY age
+ORDER BY age;
